7eba7b3f1434216840ff8c6fe07ee41389744491
MervynOLuing <48838026+MervynOLuing@users.noreply.github.com>
2023-04-14 13:05:46 +0100

    Update hEDAContinuous.R

--- R/hEDAContinuous.R
+++ R/hEDAContinuous.R
@@ -98,7 +98,7 @@ hEDAContinuous<-function (frame, err, suggestions =NULL,
        levels(groups) <- c(1:length(levels(groups)))
       for (pop_i in 2:popSize) { # don't mutate the best
         population[pop_i,]<-population[1,]
-        population[pop_i,sample(vars,1)]<- as.numeric(sample(levels(group),1))
+        population[pop_i,sample(vars,1)]<- as.numeric(sample(levels(groups),1))
         }
 
 }
